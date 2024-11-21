f5be49c8a305b6643c23474d20db1a587b2c603e
hehe <user@LINUX.myguest.virtualbox.org>
2024-11-21 23:12:36 +0700

    correcting after teacher verification 2

--- Array.cpp
+++ Array.cpp
@@ -1,47 +1,63 @@
-#include <sstream>
+
+#include "header.h"
 #include <iostream>
 #include <fstream>
-#include <string>
-#include <algorithm>
+
 using namespace std;
-#include "header.h"
+
 
 
 void Array::MPUSHend(string& value) {
-    data[size++] = value;
+    data[size++] = value + " ";
 }
 
 void Array::MPUSHindex(const int& index, string& value) {
-    for (int i = size; i > index; i--) {
+    if (index < 0 || index >= size ) {
+        cout << endl << "INCORRECT INPUT!!!" << endl; //
+        return;
+    }
+    for (int i = size; i > index; i--) { 
         data[i] = data[i - 1];
     }
     data[index] = value + " ";
-    size++;
+    size++; 
 }
 
 string Array::MGETL(const int& index) {
-    return data[index];
+    if (index < 0 || index >= size ) {
+        cout << endl << "INCORRECT INPUT!!!" << endl; //
+        return "ERROR";
+    }
+    return data[index]; 
 }
 
 void Array::MDEL(const int& index) {
-    for (int i = index; i < size - 1; i++) {
+    if (index < 0 || index >= size ) {
+        cout << endl << "INCORRECT INPUT!!!" << endl; //
+        return;
+    }
+    for (int i = index; i < size - 1; i++) { 
         data[i] = data[i + 1];
     }
-    size--;
+    size--; 
 }
 
 void Array::MREPL(const int& index, string& value) {
-    data[index] = " " + value + " ";
+    if (index < 0 || index >= size ) {
+        cout << endl << "INCORRECT INPUT!!!" << endl; //
+        return;
+    }
+    data[index] = " " + value + " "; 
 }
 
 int Array::MSIZE() {
-    return size;
+    return size;  
 }
 
 void Array::MREAD() {
     cout << endl;
-    for (int i = 0; i < size; i++) {
-        cout << "[ " << data[i] << " ]" << endl;
+    for (int i = 0; i < size; i++) {  
+        cout << data[i];
     }
     cout << endl;
 }
